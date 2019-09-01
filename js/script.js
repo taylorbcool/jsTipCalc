a9bb7ffd50c55167e95e3e75686ac429c22d1dd6
Taylor Cool <taycool@gmail.com>
2019-09-01 13:18:02 -0600

    added files because I'm stupid and still don't understand git

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function calculateTip() {
+    var billAmt = document.getElementById(billAmt).value;
+    var serviceQual = document.getElementById(serviceQual).value;
+    //validate input
+    if (billAmt === "" || serviceQual == 0) {
+        alert("Please enter values");
+        return;
+    }
+    //Calculate tip
+    var total = (billAmt * serviceQual);
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total = total.toFixed(2);
+    //Display the tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+
+//click to call function
+document.getElementById("calculate").onclick = function() {
+  calculateTip();
+
+};
